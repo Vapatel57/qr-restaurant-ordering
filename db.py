d018ddf0fd13499d05775f1362c09c77aa97a198
vaidik <vapatel2005@icloud.com>
2025-12-17 15:36:12 +0530

    Use Railway persistent volume for DB

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import os
 from flask import g
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "restaurant.db")
+DB_PATH = os.getenv("DB_PATH", "/data/restaurant.db")
 
 # ---------------- DB CONNECTION ----------------
 def get_db():
